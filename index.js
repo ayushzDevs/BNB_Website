8e53a09b1b9743db6311ad81ac05e1fca37ff2a9
ayushzAIML <theayushchakraborty@gmail.com>
2026-05-10 19:28:32 +0530

    10 may

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const mongoose = require('mongoose');
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
