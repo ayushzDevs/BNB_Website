b9088360426b389d0e83ec91b30c0be7699f6310
ayushzAIML <theayushchakraborty@gmail.com>
2026-05-10 19:56:15 +0530

    10 may

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ const app = express();
 const port = 8080;
 const mongoose = require('mongoose');
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/BNB_Website"
 
+main().then(()=>{
+    console.log("Connected to MongoDB");
+})
+.catch((err)=>{
+    console.log("Error connecting to MongoDB:", err);
+});
+
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("I am root")
+})
 
 
 
---
+++ models/listing.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    location: String,
+    image: String,
+    country : String
+
+});
+
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
