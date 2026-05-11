1a6dc4d4b19225abe3eb0ddc94954376ad7fcb74
ayushzAIML <theayushchakraborty@gmail.com>
2026-05-11 19:01:58 +0530

    11 may

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const mongoose = require('mongoose');
+const Listing = require('../models/listing.js');
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/BNB_Website"
 
@@ -31,7 +33,17 @@ app.get("/",(req,res)=>{
 
 
 
-
+app.get("/testListening", (req,res)=>{
+    let sampleListing = new Listing({
+        title : "My New Villa",
+        description : "This is a beautiful villa located in the heart of the city.",
+        price : 1250,
+        location : "landour , Uttarakhand",
+        country : "India",
+        
+    })
+    sampleListing.save()
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
