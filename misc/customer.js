49474ddd4c8f96930dfd58cfaa17d85206ea30b0
ayushzAIML <theayushchakraborty@gmail.com>
2026-05-24 22:41:57 +0530

    24 may

--- misc/customer.js
+++ misc/customer.js
@@ -1,100 +1,72 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-
-main().then(()=>{
-    console.log("Connected to MongoDB");
-})
-.catch((err)=>{
-    console.log("Error connecting to MongoDB:", err);
-});
-
-async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/ord");
-    // await findusr();
-};
-
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const orderSchema = new Schema({
-    item : String,
+    item: String,
     price: Number,
 });
 
-Schema.pre("findOneAndDelete", async()=>{
-    console.log("pree middleware")
-})
-
-Schema.post("findOneAndDelete", async()=>{
-    console.log("post middleware")
-})
-
-const order = mongoose.model("order",orderSchema);
-
-const findorders = async()=>{
-    let res = await order.insertMany(
-        [
-            {
-                item: "item1",
-                price: 100,
-            },
-            {
-                item: "item2",
-                price: 200,
-            },
-             {
-                item: "item3",
-                price: 300,
-            },
-        ]
-    );
-    console.log(res);
-}
-
-findorders();
-
-const customer_schema = new Schema({
-    name : String,
-    orders : [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "order",
-    }]
+const customerSchema = new Schema({
+    name: String,
+    orders: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "order",
+        },
+    ],
 });
 
-const customer = mongoose.model("customer",customer_schema);
 
-const findcustomer = async()=>{
-    let cust1 = await customer.find({}).populate("orders");
-    console.log(cust1[0]);
+customerSchema.pre("findOneAndDelete",async ()=>{
+    console.log("Pre hook called before deleting a customer");
+})
+
+customerSchema.post("findOneAndDelete",async ()=>{
+    console.log("Post hook called after deleting a customer");
+})
+
+const Order = mongoose.model("order", orderSchema);
+const Customer = mongoose.model("customer", customerSchema);
+
+const addCustomerWithOrder = async () => {
+    const newCustomer = new Customer({
+        name: "customer1",
+    });
+
+    const newOrder = new Order({
+        item: "chicken",
+        price: 500,
+    });
+
+    newCustomer.orders.push(newOrder);
+
+    await newOrder.save();
+    await newCustomer.save();
 };
 
-findcustomer();
-
-
-const addCust = async()=>{
-    let newcust = new customer({
-    name: "customer1",
-    });
-
-    let neworder = new order({
-        item: "pizza",
-        price : 500
-    });
-
-    newcust.orders.push(neworder);
-    
-    await neworder.save();
-    await newcust.save();
-
-    console.log("Customer and order added successfully");
+const delcust = async()=>{
+    let data = await Customer.findOneAndDelete({name:"customer1"});
+    console.log(data);
 }
 
-
-addcust();
-
-// const delcust = async()=>{
-//     await customer.deleteMany({});
-//     await order.deleteMany({});
-//     console.log("All customers and orders deleted successfully");
-// }
-
 delcust();
+
+const findCustomers = async () => {
+    const customers = await Customer.find({}).populate("orders");
+    console.log(customers);
+};
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/ord");
+    await addCustomerWithOrder();
+    await findCustomers();
+    await mongoose.connection.close();
+}
+
+main()
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log("Error connecting to MongoDB:", err);
+    });
